13cdd55d4061d8ea86501493ea6cc30d106cd4d7
liuyaowen <liuyaowen_smile@126.com>
2024-04-27 15:00:38 +0800

    feat(master): 增加对fmnist数据集的支持

---
+++ dataset/datasets/partitioned_femnist.py
@@ -0,0 +1,158 @@
+import os
+
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+
+from utils.utils import find_project_root
+from .basic_dataset import FedDataset, Subset
+from dataset.utils.partition import FMNISTPartitioner
+
+
+class PartitionedFMNIST(FedDataset):
+    """:class:`FedDataset` with partitioning preprocess. For detailed partitioning, please
+    check `Federated Dataset and DataPartitioner <https://fedlab.readthedocs.io/en/master/tutorials/dataset_partition.html>`_.
+
+
+    Args:
+        root (str): Path to download raw dataset.
+        path (str): Path to save partitioned subdataset.
+        numClients (int): Number of clients.
+        download (bool): Whether to download the raw dataset.
+        preprocess (bool): Whether to preprocess the dataset.
+        partition (str, optional): Partition name. Only supports ``"noniid-#label"``, ``"noniid-labeldir"``, ``"unbalance"`` and ``"iid"`` partition schemes.
+        dirAlpha (float, optional): Dirichlet distribution parameter for non-iid partition. Only works if ``partition="dirichlet"``. Default as ``None``.
+        verbose (bool, optional): Whether to print partition process. Default as ``True``.
+        seed (int, optional): Random seed. Default as ``None``.
+        transform (callable, optional): A function/transform that takes in an PIL image and returns a transformed version.
+        targetTransform (callable, optional): A function/transform that takes in the target and transforms it.
+    """
+
+    def __init__(self,
+                 numClients,
+                 dataName="femnist",
+                 root=os.path.join(find_project_root(), "data"),
+                 path="./clientdata",
+                 data_preprocessed=False,
+                 download=True,
+                 partition="iid",
+                 majorClassesNum=1,
+                 dirAlpha=None,
+                 verbose=True,
+                 seed=None,
+                 transform=None,
+                 targetTransform=None) -> None:
+
+        self.partitioner = None
+        self.root = os.path.expanduser(root)
+        self.path = path
+        self.numClients = numClients
+        self.transform = transform
+        self.targetTransform = targetTransform
+        self.dataName = dataName
+
+        # image
+        self.trainDatasets = torchvision.datasets.FashionMNIST(root=self.root,
+                                                               train=True,
+                                                               download=download)
+        self.testDatasets = torchvision.datasets.FashionMNIST(root=self.root,
+                                                              train=False, transform=self.transform,
+                                                              target_transform=self.targetTransform,
+                                                              download=download)
+
+        if data_preprocessed:
+            self.preprocess(partition=partition,
+                            major_classes_num=majorClassesNum,
+                            dir_alpha=dirAlpha,
+                            verbose=verbose,
+                            seed=seed)
+
+    def preprocess(self,
+                   partition="iid",
+                   major_classes_num=1,
+                   dir_alpha=None,
+                   verbose=True,
+                   seed=None):
+        """Perform FL partition on the dataset, and save each subset for each client into ``data{cid}.pkl`` file.
+
+        For details of partition schemes, please check `Federated Dataset and DataPartitioner <https://fedlab.readthedocs.io/en/master/tutorials/dataset_partition.html>`_.
+        """
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+            os.makedirs(os.path.join(self.path, "train"))
+            # os.mkdir(os.path.join(self.path, "var"))
+            # os.mkdir(os.path.join(self.path, "test"))
+
+        self.partitioner = FMNISTPartitioner(self.trainDatasets.targets,
+                                            self.numClients,
+                                            partition=partition,
+                                            major_classes_num=major_classes_num,
+                                            dir_alpha=dir_alpha,
+                                            verbose=verbose,
+                                            seed=seed)
+
+        # partition
+        subsets = {
+            cid: Subset(self.trainDatasets,
+                        self.partitioner.client_dict[cid],
+                        transform=self.transform,
+                        target_transform=self.targetTransform)
+            for cid in range(self.numClients)
+        }
+        for cid in subsets:
+            torch.save(
+                subsets[cid],
+                os.path.join(self.path, "train", "data{}.pkl".format(cid)))
+
+    def getDataset(self, cid=None, type_="train"):
+        """Load subdataset for client with client ID ``cid`` from local file.
+
+        Args:
+             cid (int): client id
+             type_ (str, optional): Dataset type, can be ``"train"``, ``"val"`` or ``"test"``. Default as ``"train"``.
+
+        Returns:
+            Dataset
+        """
+        if cid is None:
+            if type_ == "test":
+                return self.testDatasets
+            elif type_ == "train":
+                return self.trainDatasets
+        if type_ == "train":
+            return torch.load(os.path.join(self.path, type_, "data{}.pkl".format(cid)))
+        elif type_ == "test":
+            # todo 每个客户端的测试集
+            pass
+        else:
+            pass
+            # TODO 验证集
+
+    def getDataloader(self, cid=None, batch_size=None, type_="train"):
+        """Return dataload for client with client ID ``cid``.
+
+        Args:
+            cid (int): client id
+            batch_size (int, optional): batch size in DataLoader.
+            type (str, optional): Dataset type, can be ``"train"``, ``"val"`` or ``"test"``. Default as ``"train"``.
+        """
+        data_loader = None
+        # 返回整体训练与测试集
+        if cid is None:
+            if type_ == "test":
+                return DataLoader(self.testDatasets,
+                                  batch_size=len(self.testDatasets) if batch_size is None else batch_size)
+            elif type_ == "train":
+                return DataLoader(self.trainDatasets, batch_size=batch_size, shuffle=True)
+        if type_ == "train":
+            dataset = self.getDataset(cid, type_)
+            batch_size = len(dataset) if batch_size is None else batch_size
+            data_loader = DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+        elif type_ == "test":
+            # todo 每个客户端的测试集
+            pass
+        else:
+            pass
+            # TODO 验证集
+
+        return data_loader
---
+++ fedavg/mainfmnist.py
@@ -0,0 +1,100 @@
+import argparse
+import os
+
+from torchvision.transforms import ToTensor
+
+from dataset.datasets.partitioned_femnist import PartitionedFMNIST
+from fedavg.server import FedavgServer
+from utils.utils import draw, find_project_root, setupSeed
+import pandas as pd
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+
+    # global config
+    parser.add_argument("--seed", type=int, default=925)
+    parser.add_argument("--recordId", type=str, default="femnist_dirichlet_0.5_100_0.1")
+    parser.add_argument("--device", type=str, default="cuda")
+
+    # multi process
+    parser.add_argument("--multiProcess", type=bool, default=True)
+    parser.add_argument("--numProcess", type=int, default=10)
+
+    # dataset config
+    """
+    partition = noniid-#label, majorClassesNum = 2 // 2label
+    
+    partition = noniid-labeldir, dirAlpha = 0.1 // dirichlet
+    
+    partition = iid // iid
+    
+    """
+    parser.add_argument("--datasetName", type=str, default="fmnist", choices=["fmnist"])
+    parser.add_argument("--partition", type=str, default="noniid-#label",
+                        choices=["noniid-#label", "noniid-labeldir", "iid"])
+    parser.add_argument("--majorClassesNum", type=int, default=2)
+    parser.add_argument("--dirAlpha", type=int, default=0.1)
+
+    # client config
+    parser.add_argument("--model", type=str, default="CnnWithBatchFormer",
+                        choices=["CnnWithBatchFormer", "CnnWithFusion"])
+    parser.add_argument("--modelConfig", type=dict,
+                        default={"backbone": "FedAvgCNN", "modelConfig": {"dataset": "fmnist"}})
+    parser.add_argument("--batchSize", type=int, default=64)
+    parser.add_argument("--localEpochs", type=int, default=10)
+    parser.add_argument("--optimizer", type=str, default="torch.optim.Adam",
+                        choices=["torch.optim.SGD", "torch.optim.Adam"])
+    parser.add_argument("--optimConfig", type=dict, default={"lr": 0.001, "betas": (0.9, 0.999), "eps": 1e-8})
+    # server config
+    parser.add_argument("--fraction", type=float, default=0.1)
+    parser.add_argument("--numClients", type=int, default=100)
+    parser.add_argument("--numGlobalEpochs", type=int, default=50)
+
+    args = parser.parse_args()
+    return args
+
+
+def run(configs, data_preprocessed=True):
+    dataPartitioner = PartitionedFMNIST(numClients=configs.numClients, dataName=configs.datasetName,
+                                       path="./clientdata/fmnist", data_preprocessed=data_preprocessed, download=True,
+                                       partition=configs.partition, majorClassesNum=configs.majorClassesNum,
+                                       dirAlpha=configs.dirAlpha, verbose=True, seed=None,
+                                       transform=ToTensor())
+
+    if data_preprocessed:
+        draw(dataPartitioner, "./clientdata/fmnist/datainfo")
+
+    print(f"Trains information: {configs.model} on {configs.datasetName} with {configs.partition} partition.")
+
+    federatedServer = FedavgServer(dataPartitioner, configs)
+
+    results = federatedServer.train()
+
+    # save results
+    root = find_project_root()
+    recordPath = os.path.join(root, f"results/{configs.model}/{configs.datasetName}")
+    if not os.path.exists(recordPath):
+        os.makedirs(recordPath)
+
+    dataFrame = pd.DataFrame(results, columns=["loss", "accuracy"])
+
+    dataFrame.to_csv(os.path.join(recordPath, f"{configs.recordId}.csv"), index_label="epoch")
+
+
+if __name__ == '__main__':
+    configs = arg_parse()
+
+    #
+    # configs.model = "CnnWithFusion"
+    # configs.modelConfig = {"backbone": "FedAvgCNN", "modelConfig": {"dataset": "fmnist"}}
+    # for i in range(10):
+    #     configs.recordId = f"CnnWithFusion[multi]_fmnist_noniid-#label_2_100_0.1_{i}"
+    #     run(configs, data_preprocessed=True)
+
+    configs.model = "CnnWithBatchFormer"
+    configs.modelConfig = {"backbone": "FedAvgCNN", "modelConfig": {"dataset": "fmnist"}}
+    for i in range(10):
+        configs.recordId = f"CnnWithBatchFormer[10chanel]_fmnist_noniid-#label_2_100_0.1_{i}"
+        run(configs, data_preprocessed=True)
+
